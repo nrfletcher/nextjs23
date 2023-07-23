0287d537f4251e8f867e3e457f453bb7d76bb576
riley <nrileyfletcher@gmail.com>
2023-07-23 01:10:52 -0400

    styles

--- nextjs-blog/pages/first-post.js
+++ nextjs-blog/pages/first-post.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import Image from 'next/image';
+import Head from "next/head";
+import Script from 'next/script';
+import Layout from "../components/layout";
 
 export default function FirstPost() {
     return (
-        <div>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>"What's good?"</h1>
             <h2>
                 <Link href="/">Home</Link>
             </h2>
-        </div>
+        </Layout>
     )   
 }
