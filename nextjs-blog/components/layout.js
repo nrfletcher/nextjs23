67646e8f2e1e23866d446dc24579b33d84b9d940
riley <nrileyfletcher@gmail.com>
2024-01-25 10:07:02 -0500

    feature branch

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Riley';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Next.js Website';
 
 export default function Layout({ children, home }) {
   return (
